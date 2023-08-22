6ff4296af009817aa9791ff9f8f356f9f942635c
SebasBeltranM <142366728+SebasBeltranM@users.noreply.github.com>
2023-08-22 17:53:59 -0500

    :pencil2: Creación atributos y métodos

--- co.edu.uniquindio.biblioteca/src/main/java/co/edu/uniquindio/biblioteca/model/Libro.java
+++ co.edu.uniquindio.biblioteca/src/main/java/co/edu/uniquindio/biblioteca/model/Libro.java
@@ -1,4 +1,33 @@
 package co.edu.uniquindio.biblioteca.model;
 
-public class Libro {
+    public class Libro {
+    //Atributos
+        public String title;
+        public String gender;
+        public String author;
+        public Integer publicationYear;
+        public String editorial;
+        public Integer pageNumber;
+
+    //Métodos
+        public String getTitle(){
+            return this.title;
+        }
+        public String getGender(){
+            return this.gender;
+        }
+        public String getAuthor(){
+            return this.author;
+        }
+        public Integer getPublicationYear(){
+            return this.publicationYear;
+        }
+        public String getEditorial(){
+            return this.editorial;
+        }
+        public Integer getPageNumber(){
+            return this.pageNumber;
+        }
+
+
 }
